e6381599a22395d28819a1d42487ecd61292a8d3
v1souto <souto2096@gmail.com>
2026-06-02 12:34:31 -0300

    ss

---
+++ rich/rich001.py
@@ -0,0 +1,4 @@
+from rich import print
+print('Olá, [red]Mundo[/]! :earth_americas:')
+print('Olá, [bold blue on white]Pequeno Gafanhoto[/] :vulcan_salute:')
+print('[red] python -m rich.emoji[/]') # para saber os emejis
