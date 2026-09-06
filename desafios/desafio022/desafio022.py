6259dafc510ace00b79a31d11b2dbe04280e6d6a
v1souto <souto2096@gmail.com>
2026-09-06 17:45:34 -0300

    ss

--- desafios/desafio022/desafio022.py
+++ desafios/desafio022/desafio022.py
@@ -7,7 +7,7 @@ class ControleRemoto:
     volume_min:int = 1
     volume_max:int = 5
 
-    def __init(self, canal = 1, volume = 2):
+    def __init__(self, canal = 1, volume = 2):
         self.canal_atual:int = canal
         self.volume_atual:int = volume
         self.ligado:bool = False
@@ -78,5 +78,5 @@ while True:
         case '-':
             c.volume_menos()
         case '+':
-            c.colume_mais()
+            c.volume_mais()
     print("\n" * 10)
